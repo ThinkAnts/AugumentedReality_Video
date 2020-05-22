633245e32d2c1daeac6032367a2cecf695d9f6de
ravi.kishore <ravi.kishore@indegene.com>
2020-05-22 09:38:06 +0530

    Initial Commit

---
+++ OmniPresence_AR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OmniPresence_AR
+//
+//  Created by Ravi kishore on 5/22/20.
+//  Copyright © 2020 Indegene. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
